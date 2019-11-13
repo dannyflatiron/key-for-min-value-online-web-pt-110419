ecc5aaebb07ee5832edc046eb2b56e81a3dfb3e7
Daniel Reina <dannyflatiron@gmail.com>
2019-11-13 00:53:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 require 'pry'
 def key_for_min_value(name_hash)
-  array = []
-  array << name_hash.to_a
-  array.each do |n|
+  min_key = nil
+  lowest_value = nil
+  name_hash.each do |n, o|
+
 
   end
 # binding.pry
