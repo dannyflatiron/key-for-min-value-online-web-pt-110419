2145756473ae9c326db0befb59b436277016e72e
Daniel Reina <dannyflatiron@gmail.com>
2019-11-13 01:14:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   min_key = nil
   lowest_value = nil
   name_hash.each do |n, o|
-    if lowest_value == nil || o < lowest_value
+    if min_key == nil || o < lowest_value
       lowest_value = o
       min_key = n
     end
