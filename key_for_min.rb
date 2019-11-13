4df68c53fa8ffed37d685d660a97439fe1f18dc4
Daniel Reina <dannyflatiron@gmail.com>
2019-11-13 01:13:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,15 @@ def key_for_min_value(name_hash)
   min_key = nil
   lowest_value = nil
   name_hash.each do |n, o|
-
-
+    if lowest_value == nil || o < lowest_value
+      lowest_value = o
+      min_key = n
+    end
+    min_key
   end
 # binding.pry
 end
+
+#Created two variables since we have to store the values for the keys and values from the hash
+#Both variables should be set to nil since the method should return nil if hash is empty
+#lowest
